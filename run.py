9eda8d679a0d280d978ed38212001539938b9de8
kachornpat <kchornpg@gmail.com>
2024-09-26 09:51:30 +0700

    add project page

--- run.py
+++ run.py
@@ -3,10 +3,28 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+project_list = [
+    {
+        "id": 1,
+        "name": "Marbel"
+    },
+    {
+        "id": 2,
+        "name": "Atlas"
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template("home.html", title='Home')
+    return render_template("home.html", title="Home", project_list=project_list)
+
+
+@app.route("/project/<int:project_id>")
+def project(project_id):
+    return render_template("project.html", title="Project", project_id=project_id)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
