62424457027cc6ea847415536c1a7b0a9599b1a2
kachornpat <kchornpg@gmail.com>
2024-10-02 21:28:27 +0700

    update: add login, register

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '6b71aa6324cb02548a704cf0a1c0db17'
+
 
 project_list = [{"id": 1, "name": "Marbel"}, {"id": 2, "name": "Atlas"}]
-
-
 task_list = [{"id": 1, "name": "Model"}, {"id": 2, "name": "Sculpt"}]
-
 
 @app.route("/")
 @app.route("/home")
@@ -16,20 +16,31 @@ def home():
         "home.html", title="Home", project_list=project_list, task_list=task_list
     )
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register",form=form)
 
-@app.route("/project")
-def project():
-    return render_template("project_detail.html", title="Project", project_list=project_list)
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Log in", form=form)
 
 
 @app.route("/project/<int:project_id>")
 def project_detail(project_id):
-    return render_template("project_detail.html", title="Project", project_id=project_id)
 
+    project_name = ""
 
-@app.route("/task")
-def task():
-    return render_template("task_detail.html", title="Task", task_list=task_list)
+    for project in project_list:
+        if project['id'] == project_id:
+            project_name = project['name']
+    return render_template(
+        "project_detail.html",
+        title="Project",
+        project_name=project_name,
+        task_list=task_list,
+    )
 
 
 @app.route("/task/<int:task_id>")
